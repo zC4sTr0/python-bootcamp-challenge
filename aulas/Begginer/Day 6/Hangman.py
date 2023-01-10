fdcbfaaff68f6b6154d758fa151ad2a306e4d862
Gabriel Castro <c4str0@live.com>
2023-01-10 06:37:12 -0300

    Day 6 - Hangman Exercise - Created guess letter loop

--- aulas/Begginer/Day 6/Hangman.py
+++ aulas/Begginer/Day 6/Hangman.py
@@ -1,12 +1,22 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel", "stark", "joaozinho", "gabriel"]
+word_list = ["aardvark", "baboon", "camel", "stark",
+             "joaozinho", "gabriel", "apple", "banana"]
+chosen_word = random.choice(word_list)
+display_word = chosen_word
 
-letter = input("Guess a letter: ").lower()
+# Testing code
+print(f'DEBUG: The word choosen is: "{chosen_word}".')
 
-for word in word_list:
-    position = word.find(letter)
-    if (position != -1):
-        print("Found in word: " + word)
-    else:
-        print("Not found in word: " + word)
+for letter_display in chosen_word:
+    print("_", end=" ")
+    display_word = display_word.replace(letter_display, "_")
+
+letter_guesses = input("\nGuess a letter: ").lower()
+
+for letter in range(len(chosen_word)):
+    if letter_guesses == chosen_word[letter]:
+        display_word = display_word[:letter] + \
+            letter_guesses + display_word[letter + 1:]
+
+print(display_word)
