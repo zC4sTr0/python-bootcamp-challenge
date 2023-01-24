1fca6128dee41bd41b0e9a3964562db41ab471f9
Gabriel Castro <c4str0@live.com>
2023-01-23 23:40:30 -0300

    Day 8 - Functions

---
+++ aulas/Begginer/Day 8/functionGreet.py
@@ -0,0 +1,8 @@
+def saudacoes(nome, localidade):
+    print(f"Olá {nome}! você é da região de {localidade}.")
+    print("Bem vindo ao curso de Python!")
+    print("Espero que você aproveite bastante!")
+    print("Tenha um ótimo dia!\n")
+
+
+saudacoes("Joãozinho", "São Paulo")
